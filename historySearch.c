34275fea054155fc6509af263e8171153cf9d794
Tyler Tucker <tylermatthewtucker@gmail.com>
2021-03-25 19:33:54 -0500

    Updated historySearch

--- historySearch.c
+++ historySearch.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 void searchHistory(char *findItem);
 
 void main()
 {
-    char name[255]="Michael is a";
+    char name[255]="ThOMas is ";
     searchHistory(name)  ;
 }
 
@@ -28,12 +29,13 @@ void searchHistory(char *findItem){
         exit(EXIT_FAILURE);
     }
 
-    //copies the contents in the file to a character array line
+    //copies the contents in the file to a character buffer
 	while(fgets(buffer, bufferLength, fpointer))
 	{
 	    //substring of the buffer and the string item to search
 	    //and checks if the search string is in file
-		char *ptr = strstr(buffer, findItem);
+	    //THE StrStrIA is a function that ignores case when comparing strings
+		char *ptr = StrStrIA(buffer,findItem);
 		if (ptr != NULL)
 		{
 			wordExists=1;
@@ -49,7 +51,7 @@ void searchHistory(char *findItem){
 	}
 	else
 	{
-		printf("String doesn't exist.\n");
+		printf("String does not exist.\n");
 		printf("Will add strings to history file...\n");
 
 	}
